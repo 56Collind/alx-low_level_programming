c7d0a94eccf65e3c99b43ecab2685b65b61bc0e7
56Collind <lincolnb2012@gmail.com>
2022-09-09 15:09:11 -0700

    My life is good

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Three alphabet dot c
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+	char danny;
+
+	for (danny = 'a'; danny <= 'z'; danny++)
+		putchar(danny);
+	for (danny = 'A'; danny <= 'Z'; danny++)
+		putchar(danny)
+			putchar('\n');
+	return (0);
+}
