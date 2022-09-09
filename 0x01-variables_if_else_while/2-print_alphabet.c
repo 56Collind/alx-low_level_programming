9a486d9c81f5a323c662d337ed3ec759deeabf24
56Collind <lincolnb2012@gmail.com>
2022-09-09 14:49:57 -0700

    My first vim

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Two alphabet dot c
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char baby;
+
+	for (baby = 'a'; baby <= 'z'; baby++)
+		putchar(baby);
+	putchar('\n');
+	return (0);
+}
