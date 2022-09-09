c5bec1b48257f7aca8ff22e09273b41616cb47e3
56Collind <lincolnb2012@gmail.com>
2022-09-09 16:07:38 -0700

    hey i am here

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - How can it be
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+		putchar('\n');
+
+	return (0);
+}
