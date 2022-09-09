44d382340b49e7ea0e30c2bb85f244039bc63f78
56Collind <lincolnb2012@gmail.com>
2022-09-09 15:59:12 -0700

    My love is okay

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - C programming is cool
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+
+	return (0);
+}
+
