e754404562d52e6dffe5d92936815e16caf5c2c6
56Collind <lincolnb2012@gmail.com>
2022-09-09 17:14:06 -0700

    My first push

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = 0; g < 100; g++)
+	{
+		putchar((g / 10) + '0');
+		putchar((g % 10) + '0');
+		if (g != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
