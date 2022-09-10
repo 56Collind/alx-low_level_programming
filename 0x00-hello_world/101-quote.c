f536d4b3f24fefd1d361652d08507ca45558fefb
56Collind <lincolnb2012@gmail.com>
2022-09-10 03:12:45 -0700

    M

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - program that print exactly and that piece of art is useful
- *
- * Description: print Dora's quote
+ * main - Entry point
  *
  * Return: Always 1 (success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
