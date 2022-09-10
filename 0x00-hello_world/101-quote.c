4cb7021e28cf149994d9724aea0fd9833d8b64c6
56Collind <lincolnb2012@gmail.com>
2022-09-10 00:57:15 -0700

    My first push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - program that print exactly and that piece of art is useful
  *
+ * Description: print Dora's quote
+ *
  * Return: Always 1 (success)
  */
+
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
