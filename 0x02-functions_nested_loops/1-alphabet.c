9d9773c17d6c7fad1b5b4b2ab5b2a55c4b3e62e2
56Collind <lincolnb2012@gmail.com>
2022-09-13 09:28:55 -0700

    My first push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet in lower cases
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lower;
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		_putchar(lower);
+		_putchar("\n");
+	}
+	return (0);
+}
