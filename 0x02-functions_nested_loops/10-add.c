5af48eee7fab45da7851d35e38dc3a4a40c1e5c4
56Collind <lincolnb2012@gmail.com>
2022-09-13 21:10:37 -0700

    My first push

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * @a: An integer a
+ * @b: An integer b
+ * Description: adds two integers and returnss the said results
+ * Return: the result of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
