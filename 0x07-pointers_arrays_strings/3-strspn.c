98fddfa63102b7e54bffae189719c0167bb118e3
56Collind <lincolnb2012@gmail.com>
2022-09-27 16:26:40 -0700

    my first commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,7 +6,7 @@
  * @accept: accept bytes
  * Return: the number accepted bytes
  */
-insigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int a, b, c;
 
