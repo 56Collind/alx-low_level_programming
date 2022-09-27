01b82d535872e1e0b23b690e97577a1006960722
56Collind <lincolnb2012@gmail.com>
2022-09-27 16:33:25 -0700

    my first commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,21 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int a, b, c;
+	unsigned int a, b, bool;
 
 	for (a = 0; *(s + a) != '\0'; a++)
 	{
-		c = a;
+		bool = 1;
 		for (b = 0; *(accept + b) != '\0'; b++)
 		{
 			if (*(s + a) == *(accept + b))
 			{
-				c = 0;
+				bool = 0;
 
 				break;
 			}
 		}
-		if (c == 1)
+		if (bool == 1)
 			break;
 	}
 	return (a);
