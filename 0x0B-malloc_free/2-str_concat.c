336b5caeba0c9753eae43b93a4afa4e9a4791b58
56Collind <lincolnb2012@gmail.com>
2022-10-05 23:13:39 -0700

    my fust commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string.
+ *
+ * Return: pointer of an array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *strout;
+	unsigned int m, n, k limit;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (m = 0; s1[m] != '\0'; m++)
+		;
+
+	for (n = 0; s2[n] != '\0'; n++)
+		;
+
+	strout = malloc(sizeof(char) * (m + n + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (k = 0; k < 1; k++)
+		strout[k] = s1[k];
+
+	limit = n;
+	for (n = 0; n <= limit; k++, n++)
+		strout[k] = s2[n];
+
+	return (strout);
+}
