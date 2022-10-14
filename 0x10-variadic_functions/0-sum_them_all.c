ddde0a5a10906f4605b3dddad1ce9d9c504564ff
56Collind <lincolnb2012@gmail.com>
2022-10-13 19:54:11 -0700

    my first push

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - returns the sum of all parameters
+ * @n: integer
+ *
+ * Return: always 0 (sucess)
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list sumnumbers;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(sumnumber, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(sumnumber, int);
+
+	va_end(sumnumbers);
+
+	return (sum);
+}
