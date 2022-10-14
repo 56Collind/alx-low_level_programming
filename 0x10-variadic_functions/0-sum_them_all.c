cd57f662701d11eff26aeecccf67ceeb7445aca9
56Collind <lincolnb2012@gmail.com>
2022-10-13 20:00:43 -0700

    my first push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	va_start(sumnumber, n);
+	va_start(sumnumbers, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(sumnumber, int);
+		sum += va_arg(sumnumbers, int);
 
 	va_end(sumnumbers);
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,7 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
-void print strings9const char *seperator, const unsigned int n, ...);
+void print strings(const char *seperator, const unsigned int n, ...)
 void print_all(const char * const format, ...);
+
 #endif
