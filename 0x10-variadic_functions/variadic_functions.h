f03ff6ff57b16ce57cd1eaab71f1a6df65e6dca9
56Collind <lincolnb2012@gmail.com>
2022-10-14 21:37:31 -0700

    my push

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,8 +5,6 @@
 #include <stdio.h>
 
 int sum_them_all(const unsigned int n, ..);
-void print_numbers(const char *seperator, const unsigned int n, ...);
-void print strings(const char *seperator, const unsigned int n, ...)
-void print_all(const char * const format, ...);
+
 
 #endif
