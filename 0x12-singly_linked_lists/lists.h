f16ebacd2a37ef58141de7efb2365b92bc4ff1c1
56Collind <lincolnb2012@gmail.com>
2022-10-21 19:49:21 -0700

    my commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,8 @@ typeddef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-
+size_t list_len(cost list_t *h);
+list_t *add_node(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
