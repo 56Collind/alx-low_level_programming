3a7b96722bc1f837ea6db20d97a383d2acdf766e
56Collind <lincolnb2012@gmail.com>
2022-10-28 20:38:02 -0700

    my commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - get the value of abit at a given index
+ * @n: number to evaluate
+ * @index: index starting from 0, of the bit we want to get
+ * Return: value of bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int hold;
+
+	if (index > 64)
+		return (-1);
+
+	head = n >> index;
+
+	return (hold & 1);
+}
